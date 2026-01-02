3d3a74df47661ec6aebee2d9b1f236406c1c6d23
Arkhip Ivanchenko <ortiz47@mail.ru>
2026-01-02 11:02:41 +0300

    overseer register stubs

--- skylr-overseer/internal/overseer/overseer.go
+++ skylr-overseer/internal/overseer/overseer.go
@@ -2,12 +2,52 @@ package overseer
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/cutlery47/skylr/skylr-overseer/internal/pkg/utils"
 )
 
 type Overseer struct {
+	shards   []string // shards ipv4 addrs
+	shardsMu *sync.RWMutex
 }
 
 func (ovr *Overseer) Register(ctx context.Context, addr string) error {
-	return errors.New("not implemented")
+	if err := utils.CtxDone(ctx); err != nil {
+		return err
+	}
+
+	err := ovr.addShard(addr)
+	if err != nil {
+		return fmt.Errorf("addShard: %w", err)
+	}
+
+	err = ovr.Reshard(ctx)
+	if err != nil {
+		return fmt.Errorf("Reshard: %w", err)
+	}
+
+	return nil
+}
+
+func (ovr *Overseer) addShard(addr string) error {
+	ovr.shardsMu.Lock()
+	defer ovr.shardsMu.Unlock()
+
+	ovr.shards = append(ovr.shards, addr)
+	log.Printf("[INFO] New shard added successfully! Current shards: %v\n", ovr.shards)
+
+	return nil
+}
+
+func (ovr *Overseer) Reshard(ctx context.Context) error {
+	if err := utils.CtxDone(ctx); err != nil {
+		return err
+	}
+
+	log.Println("[INFO]: Reshard: resharding to be impl here")
+
+	return nil
 }
